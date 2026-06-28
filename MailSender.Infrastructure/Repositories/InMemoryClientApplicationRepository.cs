95b061952cacda41ca026e2fdf9ac98d3ef4a943
Magda-K <mkapusta2003@gmail.com>
2026-06-28 21:11:36 +0200

    mail log cd

--- MailSender.Infrastructure/Repositories/InMemoryClientApplicationRepository.cs
+++ MailSender.Infrastructure/Repositories/InMemoryClientApplicationRepository.cs
@@ -10,14 +10,26 @@ public class InMemoryClientApplicationRepository : IClientApplicationRepository
     public Task AddAsync(ClientApplication application)
     {
         _applications[application.AppId] = application;
-
         return Task.CompletedTask;
     }
 
     public Task<ClientApplication?> GetByAppIdAsync(string appId)
     {
         _applications.TryGetValue(appId, out var application);
+        return Task.FromResult(application);
+    }
 
+    public Task<ClientApplication?> GetByIdAsync(Guid id)
+    {
+        var application = _applications.Values
+            .FirstOrDefault(a => a.Id == id);
+        return Task.FromResult(application);
+    }
+
+    public Task<ClientApplication?> GetByAppNameAsync(string appName)
+    {
+        var application = _applications.Values
+            .FirstOrDefault(a => a.AppName == appName);
         return Task.FromResult(application);
     }
 }
